e2eb52c1257c8bb5b1609f685e0575c18ba62268
Robert <robert.szalapak@gmail.com>
2025-09-07 16:37:19 +0200

    problems with sorting

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -11,31 +11,31 @@ function applyCustomSort() {
       typeof compareFunction === 'function'
         ? compareFunction
         : (a, b) => {
-          const aCode = String(a).charCodeAt(0);
-          const bCode = String(b).charCodeAt(0);
+            const aCode = String(a).charCodeAt(0);
+            const bCode = String(b).charCodeAt(0);
 
-          if (a === undefined) {
-            return 1;
-          }
+            if (a === undefined) {
+              return 1;
+            }
 
-          if (b === undefined) {
-            return -1;
-          }
+            if (b === undefined) {
+              return -1;
+            }
 
-          if (a === undefined && b === undefined) {
-            return 0;
-          }
+            if (a === undefined && b === undefined) {
+              return 0;
+            }
 
-          if (aCode < bCode) {
-            return -1;
-          }
+            if (aCode < bCode) {
+              return -1;
+            }
 
-          if (aCode > bCode) {
-            return 1;
-          }
+            if (aCode > bCode) {
+              return 1;
+            }
 
-    return String(a).localeCompare(String(b));
-        };
+            return String(a).localeCompare(String(b));
+          };
 
     for (let i = 0; i < array.length; i++) {
       for (let y = i + 1; y < array.length; y++) {
