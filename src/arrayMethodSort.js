4ef147106072ea4717b581ade314b2c50698d72a
Robert <robert.szalapak@gmail.com>
2025-09-07 16:30:50 +0200

    problems with sorting

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -8,48 +8,47 @@ function applyCustomSort() {
     const array = this;
 
     const compareFunctionHelper =
-      typeof compareFunction === 'function' ||
-      ((a, b) => {
-        if (a === undefined && b !== undefined) {
-          return 1;
-        }
+      typeof compareFunction === 'function'
+        ? compareFunction
+        : (a, b) => {
+          const aCode = String(a).charCodeAt(0);
+          const bCode = String(b).charCodeAt(0);
 
-        if (a !== undefined && b === undefined) {
-          return -1;
-        }
+          if (a === undefined) {
+            return 1;
+          }
 
-        if (a === undefined && b === undefined) {
-          return 0;
-        }
+          if (b === undefined) {
+            return -1;
+          }
 
-        const aString = String(a);
-        const bString = String(b);
+          if (a === undefined && b === undefined) {
+            return 0;
+          }
 
-        const aCode = aString.charCodeAt(0);
-        const bCode = bString.charCodeAt(0);
+          if (aCode < bCode) {
+            return -1;
+          }
 
-        if (aCode < bCode) {
-          return -1;
-        }
+          if (aCode > bCode) {
+            return 1;
+          }
 
-        if (aCode > bCode) {
-          return 1;
-        }
-
-        return String(a).localeCompare(String(b));
-      });
+    return String(a).localeCompare(String(b));
+        };
 
     for (let i = 0; i < array.length; i++) {
       for (let y = i + 1; y < array.length; y++) {
-        if (compareFunctionHelper(array[i], array[y]) > 0) {
+        const hasI = i in array;
+        const hasY = y in array;
+        const a = hasI ? array[i] : undefined;
+        const b = hasY ? array[y] : undefined;
+
+        if (compareFunctionHelper(a, b) > 0) {
           const result = array[i];
 
           array[i] = array[y];
           array[y] = result;
-        }
-
-        if (array[i] === undefined || array[y] === undefined) {
-          continue;
         }
       }
     }
