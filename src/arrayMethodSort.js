7b4d88f6e45908a4a501f6c75cbad654d5550173
Robert <robert.szalapak@gmail.com>
2025-09-07 15:50:53 +0200

    sort_method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,41 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const array = this;
+
+    const compareFunctionHelper =
+      compareFunction ||
+      ((a, b) => {
+        const aString = String(a);
+        const bString = String(b);
+
+        const aCode = aString.charCodeAt(0);
+        const bCode = bString.charCodeAt(0);
+
+        if (aCode < bCode) {
+          return -1;
+        }
+
+        if (aCode > bCode) {
+          return 1;
+        }
+
+        return aString.localeCompare(bString);
+      });
+
+    for (let i = 0; i < array.length; i++) {
+      for (let y = i + 1; y < array.length; y++) {
+        if (compareFunctionHelper(array[i], array[y]) > 0) {
+          const result = array[i];
+
+          array[i] = array[y];
+          array[y] = result;
+        }
+      }
+    }
+
+    return array;
   };
 }
 
